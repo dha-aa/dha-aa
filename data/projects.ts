ec57ac4cb887df8fcd19d2430afc7468aa829f1a
dha-aa <172045152+dha-aa@users.noreply.github.com>
2025-04-08 00:11:34 +0530

    add data/projects.ts

---
+++ data/projects.ts
@@ -0,0 +1,10 @@
+// data/projects.ts
+export const projects = [
+    { name: "git-single", tech: "you can fetch specefic dir for git repo", link: "https://github.com/dha-aa/git-single.git" },
+  ];
+  
+  export const contributions = [
+    { name: "OpenAI Prompt Collection", org: "OpenAI Community", link: "#" },
+    { name: "Next.js Boilerplate", org: "Vercel", link: "#" },
+  ];
+  
